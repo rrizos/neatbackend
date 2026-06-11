042ef8e898c62402296eb3f4e68b7fb4bdee532d
rrizos <rizos@netnest.net>
2026-06-11 10:53:19 +0300

    update project files

--- neatbackend/wsgi.py
+++ neatbackend/wsgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import logging
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neatbackend.settings')
 
+logger = logging.getLogger(__name__)
+
+
+def _run_startup_migrations():
+    if os.environ.get('RUN_MIGRATIONS_ON_STARTUP', '1') != '1':
+        return
+
+    try:
+        call_command('migrate', interactive=False, run_syncdb=True, verbosity=0)
+    except Exception:
+        logger.exception('Automatic startup migrations failed')
+        raise
+
+
+_run_startup_migrations()
 application = get_wsgi_application()
